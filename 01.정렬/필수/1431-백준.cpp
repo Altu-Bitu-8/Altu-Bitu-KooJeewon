3bacc4421628791586d34871273d55b7dff9ea53
Jeewon Koo <jeewon514@gmail.com>
2025-02-21 00:35:18 +0900

    [정렬] 2월 21일

--- 01.정렬/필수/1431-백준.cpp
+++ 01.정렬/필수/1431-백준.cpp
@@ -1,43 +1,20 @@
 #include <iostream>
-#include <algorithm>
+#include <set>
+
 using namespace std;
 
-std::string serial[51];
-
-bool userSort(std::string_view a, std::string_view b) {
-    if (a.size() != b.size()) return a.size() < b.size();
-    else {
-        int sumA{}; int sumB{};
-        for (int i = 0; i < a.size(); ++i) {
-            if ('0' <= a[i] && a[i] <= '9') sumA += a[i] - '0';
-        }
-        for (int i = 0; i < b.size(); ++i) {
-            if ('0' <= b[i] && b[i] <= '9') sumB += b[i] - '0';
-        }
-        if (sumA != sumB) return sumA < sumB;
-        return a < b;
-    }
-}
-
 int main() {
+    string A;
+    cin >> A;
 
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+    set<string> substring;
 
-    int N;
-    std::cin >> N;
-
-    for (int i = 0; i < N; ++i)
-        std::cin >> serial[i];
-
-    std::sort(serial, serial + N, userSort);
-
-    for (int i = 0; i < N; ++i)
-        std::cout << serial[i] << '\n';
-
-
-    return 0;
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = 1; j <= A.size() - i; j++) {
+            substring.insert(A.substr(i, j));
+        }
+    }
+    cout << substring.size() << '\n';
 }
 
 
